e7bcc0434c4faa85c1ba9c6e2220f2c3d529b33a
EC2 Default User <ec2-user@ip-172-16-20-123.ec2.internal>
2019-05-18 07:51:00 +0000

    Work in progress on Step 6 again. predictor_fn code

--- serve/predict.py
+++ serve/predict.py
@@ -70,16 +70,16 @@ def predict_fn(input_data, model):
     #         data_X   - A sequence of length 500 which represents the converted review
     #         data_len - The length of the review
 
-    data_X = None
-    data_len = None
+    data_X, data_X_len = convert_and_pad_data(word_dict, [review_to_words(input_data)])
+    print("Predict_fn: data_X_len: {}, data_X.shape: {}".format(data_X_len, data_X.shape))
 
     # Using data_X and data_len we construct an appropriate input tensor. Remember
     # that our model expects input data of the form 'len, review[500]'.
     data_pack = np.hstack((data_len, data_X))
     data_pack = data_pack.reshape(1, -1)
-    
-    data = torch.from_numpy(data_pack)
+
     data = data.to(device)
+    print("Predict_fn: data.shape: {}, type(data): {}".format(data.shape, type(data)))
 
     # Make sure to put the model into evaluation mode
     model.eval()
@@ -87,6 +87,7 @@ def predict_fn(input_data, model):
     # TODO: Compute the result of applying the model to the input data. The variable `result` should
     #       be a numpy array which contains a single integer which is either 1 or 0
 
-    result = None
+    with torch.no_grad():
+        result = model(data)
 
     return result
